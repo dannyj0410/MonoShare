a788dcb3d35739c82039f7895c17bda2fdbc4f3b
Daniel <dannyj172@gmail.com>
2026-05-01 01:10:58 +0300

    Add frontend api urls.

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { queryClient } from "./queryClient";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL ?? "api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
