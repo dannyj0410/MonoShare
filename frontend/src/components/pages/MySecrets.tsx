933bb25bbc1b74c8a6aade687dc202890b418557
Daniel <dannyj172@gmail.com>
2026-05-21 19:20:23 +0300

    Add dropdown icon for My Secrets sections on mobile.

--- frontend/src/components/pages/MySecrets.tsx
+++ frontend/src/components/pages/MySecrets.tsx
@@ -77,9 +77,9 @@ const MySecrets = () => {
             aria-controls="active-secrets-list"
             className={`group hover:bg-(--main-light-blue)/10 ${hiddenSections.active && "grayscale-75"} hover:rounded-md overflow-hidden cursor-pointer flex items-center h-fit justify-between mb-2 w-3xl max-md:w-full border-b-cyan-500/7 border-b max-xs:pr-0 pr-2 shadow-[0_15px_25px_-10px_rgba(6,182,212,0.15)]`}
           >
-            <div className="flex items-center gap-2 ease-in-out duration-300 relative group-hover:ml-6">
+            <div className="flex items-center gap-2 ease-in-out duration-300 relative group-hover:ml-6 max-md:ml-5">
               <ThinSimpleArrowIcon
-                className="size-4 duration-200 group-hover:opacity-100 opacity-0 absolute -left-5 ease-in-out text-(--main-light-blue)"
+                className="size-4 duration-200 group-hover:opacity-100 opacity-0 max-md:opacity-100 absolute -left-5 ease-in-out text-(--main-light-blue)"
                 isActive={hiddenSections.active}
                 rotate="-rotate-180"
               />
@@ -153,9 +153,9 @@ const MySecrets = () => {
             aria-controls="viewed-secrets-list"
             className={`group hover:bg-green-500/5 ${hiddenSections.viewed && "grayscale-75"} hover:rounded-md overflow-hidden cursor-pointer flex items-center h-fit justify-between mb-2 w-3xl max-md:w-full border-b-green-500/7 border-b max-xs:pr-0 pr-2 shadow-[0_15px_25px_-10px_rgba(34,197,94,0.15)]`}
           >
-            <div className="flex items-center gap-2 ease-in-out duration-300 relative group-hover:ml-6">
+            <div className="flex items-center gap-2 ease-in-out duration-300 relative group-hover:ml-6 max-md:ml-5">
               <ThinSimpleArrowIcon
-                className="size-4 duration-200 group-hover:opacity-100 opacity-0 absolute -left-5 ease-in-out text-green-500"
+                className="size-4 duration-200 group-hover:opacity-100 opacity-0 max-md:opacity-100 absolute -left-5 ease-in-out text-green-500"
                 isActive={hiddenSections.viewed}
                 rotate="-rotate-180"
               />
@@ -229,9 +229,9 @@ const MySecrets = () => {
             aria-controls="expired-secrets-list"
             className={`group hover:bg-red-500/5 ${hiddenSections.expired && "grayscale-75"} hover:rounded-md overflow-hidden cursor-pointer flex items-center h-fit justify-between mb-2 w-3xl max-md:w-full border-b-red-500/7 border-b max-xs:pr-0 pr-2 shadow-[0_15px_25px_-10px_rgba(239,68,68,0.3)]`}
           >
-            <div className="flex items-center gap-2 ease-in-out duration-300 relative group-hover:ml-6">
+            <div className="flex items-center gap-2 ease-in-out duration-300 relative group-hover:ml-6 max-md:ml-5">
               <ThinSimpleArrowIcon
-                className="size-4 duration-200 group-hover:opacity-100 opacity-0 absolute -left-5 ease-in-out text-red-500"
+                className="size-4 duration-200 group-hover:opacity-100 opacity-0 max-md:opacity-100 absolute -left-5 ease-in-out text-red-500"
                 isActive={hiddenSections.expired}
                 rotate="-rotate-180"
               />
