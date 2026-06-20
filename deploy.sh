7488a90afb3843ba5e3a1f03c6ce4e047d39df5a
Daniel <dannyj172@gmail.com>
2026-06-20 19:54:56 +0300

    Update deploy script to keep website running until new build is complete.

--- deploy.sh
+++ deploy.sh
@@ -11,17 +11,29 @@ echo "Installing backend dependencies..."
 cd $APP_DIR/backend
 npm ci
 npx prisma generate
-rm -rf built
+
+rm -rf built_tmp
 npm run build
+mv built built_tmp
 
 echo "Building frontend..."
 cd $APP_DIR/frontend
 npm ci
 VITE_RELEASE=$(git rev-parse --short HEAD) npm run build
 
-echo "Copying frontend to server..."
-mkdir -p $APP_DIR/backend/built/public
-cp -r dist/* $APP_DIR/backend/built/public/
+echo "Copying frontend to temporary server folder..."
+mkdir -p $APP_DIR/backend/built_tmp/public
+cp -r dist/* $APP_DIR/backend/built_tmp/public/
+
+echo "Performing atomic zero-downtime swap..."
+cd $APP_DIR/backend
+if [ -d "built" ]; then
+    mv built built_old
+    mv built_tmp built
+    rm -rf built_old
+else
+    mv built_tmp built
+fi
 
 echo "Restarting app..."
 cd $APP_DIR
